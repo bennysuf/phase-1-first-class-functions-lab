eb32bf9bf628fc8d91a3ddf57ae4f29d383a0856
Benny Sufrin <bennysuf@gmail.com>
2022-08-15 12:43:11 -0400

    commit tree e865736ec1a06750f1729a01c57944b381758677

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2,4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+ function createFareMultiplier(multiplier) {
+   return function(fare) {
+       return fare * multiplier 
+       
+   };
+}
+
+const fareDoubler = (fare) => {
+    const doubler = createFareMultiplier(2);
+    return doubler(fare);
+ } 
+const fareTripler = (fare) => {
+    const d = createFareMultiplier(3);
+    return d(fare);
+ } 
+
+function selectDifferentDrivers(drivers, fun) {
+    return fun(drivers)
+}
